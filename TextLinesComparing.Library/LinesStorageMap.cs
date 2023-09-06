41c7624c2002bca76dc857189ce50859bfebd4b8
Denis Rezepov <me@dar920910.ru>
2023-09-06 12:55:46 +0300

    Ported the LinesStorageMap class from C++ to C#.

---
+++ TextLinesComparing.Library/LinesStorageMap.cs
@@ -0,0 +1,32 @@
+namespace TextLinesComparing.Library;
+
+public class LinesStorageMap
+{
+    public readonly string Name;
+    private Dictionary<int, string> _LinesMap;
+
+    public LinesStorageMap()
+    {
+        _LinesMap = new Dictionary<int, string>();
+        Name = "Lines Storage Map";
+    }
+
+    public LinesStorageMap(LineInfo info, string name = "Lines Storage Map")
+    {
+        _LinesMap = new Dictionary<int, string>()
+        {
+            { info.Content.Key, info.Content.Value }
+        };
+        Name = name;
+    }
+
+    public void PutContent(LineInfo target_info)
+    {
+        _LinesMap.Add(target_info.Content.Key, target_info.Content.Value);
+    }
+
+    public Dictionary<int, string> Content
+    {
+        get { return _LinesMap; }
+    }
+};
---
+++ TextLinesComparing.Testing/LinesStorageMapTest.cs
@@ -0,0 +1,80 @@
+using TextLinesComparing.Library;
+
+namespace TextLinesComparing.Testing;
+
+public class LinesStorageMapTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void PutContent_TestCase_1()
+    {
+        const string example = "1111111111";
+        LineInfo info = new(example);
+
+        Assert.That(
+            actual: new LinesStorageMap(info).Content,
+            expression: Is.EqualTo(
+                new Dictionary<int, string>()
+                {
+                    { info.Content.Key, info.Content.Value }
+                }
+            )
+        );
+    }
+
+    [Test]
+    public void PutContent_TestCase_2()
+    {
+        const string example_1 = "1111111111";
+        const string example_2 = "2222222222";
+
+        LineInfo info_1 = new(example_1);
+        LineInfo info_2 = new(example_2);
+
+        LinesStorageMap map = new();
+        map.PutContent(info_1);
+        map.PutContent(info_2);
+
+        Assert.That(actual: map.Content,
+            expression: Is.EqualTo(
+                new Dictionary<int, string>()
+                {
+                    { info_1.Content.Key, info_1.Content.Value },
+                    { info_2.Content.Key, info_2.Content.Value }
+                }
+            )
+        );
+    }
+
+    [Test]
+    public void PutContent_TestCase_3()
+    {
+        const string example_1 = "1111111111";
+        const string example_2 = "2222222222";
+        const string example_3 = "3333333333";
+
+        LineInfo info_1 = new(example_1);
+        LineInfo info_2 = new(example_2);
+        LineInfo info_3 = new(example_3);
+
+        LinesStorageMap map = new();
+        map.PutContent(info_1);
+        map.PutContent(info_2);
+        map.PutContent(info_3);
+
+        Assert.That(actual: map.Content,
+            expression: Is.EqualTo(
+                new Dictionary<int, string>()
+                {
+                    { info_1.Content.Key, info_1.Content.Value },
+                    { info_2.Content.Key, info_2.Content.Value },
+                    { info_3.Content.Key, info_3.Content.Value }
+                }
+            )
+        );
+    }
+}
