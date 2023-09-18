cdc786d83ddbeefe2139c76f47f3675608974c82
Denis Rezepov <me@dar920910.ru>
2023-09-18 20:14:53 +0300

    Code from AppIntelligentWorker was successfully moved to project's command-line app.

--- TextLinesComparing.App.CLI/Program.cs
+++ TextLinesComparing.App.CLI/Program.cs
@@ -1,1 +1,26 @@
-﻿TextLinesComparing.Library.AppIntelligentWorker.PerformTask(args);
+﻿using TextLinesComparing.Library;
+
+AppDatetimeService datetime = new();
+
+List<string> arguments = AppLaunchConfiguration.GetArgumentsOfSources(args);
+SourcesExplorer explorer = new(arguments);
+
+const int CriticalPerformanceThreshold = 2;
+if (arguments.Count > CriticalPerformanceThreshold)
+{
+    LinesResultView<LinesStorageMap> artifacts =
+        explorer.GetArtifactsFromSourcesAsMapBasedContent();
+    new OutputConsolePrinter().PrintArtifacts(artifacts);
+    new OutputFilePrinter().PrintArtifacts(artifacts);
+    new OutputWebPrinter().PrintArtifacts(artifacts);
+}
+else
+{
+    LinesResultView<LinesStorageSet> artifacts =
+        explorer.GetArtifactsFromSourcesAsSetBasedContent();
+    new OutputConsolePrinter().PrintArtifacts(artifacts);
+    new OutputFilePrinter().PrintArtifacts(artifacts);
+    new OutputWebPrinter().PrintArtifacts(artifacts);
+}
+
+datetime.PrintExecutionElapsedTimeMessage();
