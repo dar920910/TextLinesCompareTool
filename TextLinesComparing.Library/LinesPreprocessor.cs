82bd8b2468b2123b3e368ece863cba9d32ef3168
Denis Rezepov <me@dar920910.ru>
2023-09-26 13:44:28 +0300

    Movement the StringsCommentsHandler class to the LinesProcessor class definition.

--- TextLinesComparing.Library/LinesPreprocessor.cs
+++ TextLinesComparing.Library/LinesPreprocessor.cs
@@ -1,33 +1,143 @@
+//-----------------------------------------------------------------------
+// <copyright file="LinesPreprocessor.cs" company="Demo Projects Workshop">
+//     Copyright (c) Demo Projects Workshop. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
 namespace TextLinesComparing.Library;
 
+/// <summary>
+/// Represents possible preprocessing operations for an artifact line.
+/// </summary>
 public class LinesPreprocessor
 {
-    private readonly string _LineToProcessing;
+    private readonly string targetLineToProcessing;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LinesPreprocessor"/> class.
+    /// </summary>
+    /// <param name="string_to_processing">Source artifact string.</param>
     public LinesPreprocessor(string string_to_processing)
     {
-        _LineToProcessing = string_to_processing;
+        this.targetLineToProcessing = string_to_processing;
     }
 
+    /// <summary>
+    /// Trims a inline comment from an artifact string.
+    /// </summary>
+    /// <param name="artifactString">Source artifact string.</param>
+    /// <returns>Trimmed artifact string.</returns>
+    public static string TrimSingleStringComment(string artifactString)
+    {
+        int trimmedSubstringLength = 0;
+
+        CharEnumerator enumerator = artifactString.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            if (GeneralCharacterValidator.IsNotCommentCharacter(enumerator.Current))
+            {
+                trimmedSubstringLength++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return artifactString[..trimmedSubstringLength];
+    }
+
+    /// <summary>
+    /// Trims all wrapped comments from an artifact string.
+    /// </summary>
+    /// <param name="artifactString">Source artifact string.</param>
+    /// <returns>Trimmed artifact string.</returns>
+    public static string TrimWrappedCommentsInsideString(string artifactString)
+    {
+        List<string> detectedComments = new ();
+
+        int artifactBegIndex = 0;
+        int artifactEndIndex = artifactString.Length - 1;
+
+        int currentIndex = artifactBegIndex;
+        while (currentIndex != artifactEndIndex)
+        {
+            int commentBegSlashIndex = currentIndex;
+            if (IsCommentSlash(artifactString.ElementAt(commentBegSlashIndex)))
+            {
+                int commentBegAsteriskIndex = currentIndex + 1;
+                if (IsCommentAsterisk(artifactString.ElementAt(commentBegAsteriskIndex)))
+                {
+                    int currentCommentIndex = currentIndex;
+                    while (currentCommentIndex != artifactEndIndex)
+                    {
+                        int commentEndAsteriskIndex = currentCommentIndex;
+                        if (IsCommentAsterisk(artifactString.ElementAt(commentEndAsteriskIndex)))
+                        {
+                            int commentEndSlashIndex = currentCommentIndex + 1;
+                            if (IsCommentSlash(artifactString.ElementAt(commentEndSlashIndex)))
+                            {
+                                detectedComments.Add(artifactString[commentBegSlashIndex .. (commentEndSlashIndex + 1)]);
+                                currentIndex = commentEndSlashIndex;
+                                break;
+                            }
+                        }
+
+                        currentCommentIndex++;
+                    }
+                }
+            }
+
+            currentIndex++;
+        }
+
+        foreach (string comment in detectedComments)
+        {
+            artifactString = artifactString.Replace(comment, string.Empty);
+        }
+
+        return artifactString;
+    }
+
+    /// <summary>
+    /// Retrieves the artifact string.
+    /// </summary>
+    /// <returns>Preprocessed artifact string.</returns>
     public string RetrievePreprocessedArtifact()
     {
-        string artifact = _LineToProcessing;
+        string artifact = this.targetLineToProcessing;
 
         // WARNING:
         // This algorithm of preprocessing has the strict defined order.
         // If you swap its steps, your unit tests from LinesPreprocessorTest can fail !!!
 
         // 1. Preprocessing for C-style comments inside the string.
-        artifact = StringsCommentsHandler.TrimWrappedCommentsInsideString(artifact);
+        artifact = TrimWrappedCommentsInsideString(artifact);
+
         // 2. Preprocessing for the single line Unix-style comment in the string.
-        artifact = StringsCommentsHandler.TrimSingleStringComment(artifact);
+        artifact = TrimSingleStringComment(artifact);
+
         // 3. Preprocessing for redunant whitespaces inside the string.
         artifact = StringsWhitespacesHandler.TrimRedundantWhitespaces(artifact);
+
         // 4. Preprocessing for redunant whitespaces in the string's start.
         artifact = StringsWhitespacesHandler.TrimStartWhitespaces(artifact);
+
         // 5. Preprocessing for redunant whitespaces in the string's end.
         artifact = StringsWhitespacesHandler.TrimEndWhitespaces(artifact);
 
         return artifact;
+    }
+
+    private static bool IsCommentSlash(char artifactCharacter)
+    {
+        const char comment_slash = '/';
+        return artifactCharacter == comment_slash;
+    }
+
+    private static bool IsCommentAsterisk(char artifactCharacter)
+    {
+        const char comment_asterisk = '*';
+        return artifactCharacter == comment_asterisk;
     }
 }
--- TextLinesComparing.Testing/LinesPreprocessorTest.cs
+++ TextLinesComparing.Testing/LinesPreprocessorTest.cs
@@ -149,4 +149,92 @@ public class LinesPreprocessorTest
 
         Assert.That(actual_result, Is.EqualTo("dir1/dir2/dir3"));
     }
+
+    [Test]
+    public void DeleteSingleStringComment_TestCase()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(
+            actual: LinesPreprocessor.TrimSingleStringComment(
+                artifactString: "This is a test artifact # with a comment"),
+            expression: Is.EqualTo("This is a test artifact "));
+
+            Assert.That(
+                actual: LinesPreprocessor.TrimSingleStringComment(
+                    artifactString: "012345#6789"),
+                expression: Is.EqualTo("012345"));
+
+            Assert.That(
+                actual: LinesPreprocessor.TrimSingleStringComment(
+                    artifactString: "# This is a comment string!"),
+                expression: Is.EqualTo(""));
+        });
+    }
+
+    [Test]
+    public void DeleteAllCommentsFromString_TestCase_0()
+    {
+        const string artifact = "$/* comment */$";
+
+        string actual_artifact = LinesPreprocessor.TrimWrappedCommentsInsideString(artifact);
+        string expected_artifact = "$$";
+
+        Assert.That(actual_artifact, Is.EqualTo(expected_artifact));
+    }
+
+    [Test]
+    public void DeleteAllCommentsFromString_TestCase_1()
+    {
+        const string artifact = "0 1 /* 1 */ 2 /* 2 */ 3 4 5 6 7 /* 7 */ 8 /* 8 */ 9";
+
+        string actual_artifact = LinesPreprocessor.TrimWrappedCommentsInsideString(artifact);
+        string expected_artifact = "0 1  2  3 4 5 6 7  8  9";
+
+        Assert.That(actual_artifact, Is.EqualTo(expected_artifact));
+    }
+
+    [Test]
+    public void DeleteAllCommentsFromString_TestCase_2()
+    {
+        const string artifact = "0 1 /*step1*/ 2 /*step2*/ 3 /*step3*/ 4 5";
+
+        string actual_artifact = LinesPreprocessor.TrimWrappedCommentsInsideString(artifact);
+        string expected_artifact = "0 1  2  3  4 5";
+
+        Assert.That(actual_artifact, Is.EqualTo(expected_artifact));
+    }
+
+    [Test]
+    public void DeleteAllCommentsFromString_TestCase_3()
+    {
+        const string artifact = "/* comment */   0 1 2 3 4 5";
+
+        string actual_artifact = LinesPreprocessor.TrimWrappedCommentsInsideString(artifact);
+        string expected_artifact = "   0 1 2 3 4 5";
+
+        Assert.That(actual_artifact, Is.EqualTo(expected_artifact));
+    }
+
+    [Test]
+    public void DeleteAllCommentsFromString_TestCase_4()
+    {
+        const string artifact = "0 1 2 3 4 5   /* comment */\n";
+
+        string actual_artifact = LinesPreprocessor.TrimWrappedCommentsInsideString(artifact);
+        string expected_artifact = "0 1 2 3 4 5   \n";
+
+        Assert.That(actual_artifact, Is.EqualTo(expected_artifact));
+    }
+
+    [Test]
+    public void DeleteAllCommentsFromString_TestCase_5()
+    {
+        const string artifact = "/* comment */0 1 2 /*   comment   */ 3 4 5/* comment */\n";
+
+        string actual_artifact = LinesPreprocessor.TrimWrappedCommentsInsideString(artifact);
+        string expected_artifact = "0 1 2  3 4 5\n";
+
+        Assert.That(actual_artifact, Is.EqualTo(expected_artifact));
+    }
 }
