c39668d93ca21aba17848ef3c9fb041b0c177bfd
Denis Rezepov <me@dar920910.ru>
2023-10-02 22:06:07 +0300

    Added output for program's help and checking input arguments.

--- TextLinesComparing.App.CLI/OutputInfo.cs
+++ TextLinesComparing.App.CLI/OutputInfo.cs
@@ -7,6 +7,7 @@
 #pragma warning disable SA1600 // ElementsMustBeDocumented
 
 using TextLinesComparing.Library;
+using static System.Console;
 
 internal struct OutputInfo
 {
@@ -22,6 +23,33 @@ internal struct OutputInfo
     internal bool HasFileOutput { get; set; }
 
     internal bool HasWebOutput { get; set; }
+
+    internal static void PrintHelp()
+    {
+        const string source_1 = "./source_1.txt";
+        const string source_2 = "./source_2.txt";
+        const string source_3 = "./source_3.txt";
+        const string source_4 = "./source_4.txt";
+        const string source_5 = "./source_5.txt";
+
+        WriteLine("\n[ERROR] You should pass command-line arguments to successfully run the program.\n");
+        WriteLine("[HELP] Use the following format of the command:");
+        WriteLine("<EXECUTABLE> [OPTIONAL_PARAMETERS] <SOURCE_FILES...>");
+
+        WriteLine("\nPossible Parameters:\n");
+        WriteLine($"{ResultFileOutputParameter} : add output of results to a text file");
+        WriteLine($"{ResultWebOutputParameter} : add output of results to a web page");
+
+        WriteLine("\nCommand Examples:\n");
+        WriteLine($"dotnet run {source_1} {source_2}");
+        WriteLine($"dotnet run {ResultFileOutputParameter} {source_1} {source_2} {source_3}");
+        WriteLine($"dotnet run {ResultWebOutputParameter} {source_1} {source_2} {source_3}");
+        WriteLine(
+            $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name} " +
+                $"{ResultFileOutputParameter} {ResultWebOutputParameter}" +
+                    $"{source_1} {source_2} {source_3} {source_4} {source_5}");
+        WriteLine();
+    }
 
     internal bool IsOutputParameter(string argument)
     {
--- TextLinesComparing.App.CLI/Program.cs
+++ TextLinesComparing.App.CLI/Program.cs
@@ -9,25 +9,35 @@ using TextLinesComparing.Library;
 
 Stopwatch stopwatch = new ();
 stopwatch.Start();
-OutputInfo outputInfo = new ();
 
-List<string> sources = new ();
-
-for (int source_index = 0; source_index < args.Length; source_index++)
+if (args.Length == 0)
 {
-    string argument = args[source_index];
+    OutputInfo.PrintHelp();
+}
+else
+{
+    OutputInfo outputInfo = new ();
+    List<string> sources = new ();
 
-    if (argument.EndsWith(".exe") || outputInfo.IsOutputParameter(argument))
+    for (int source_index = 0; source_index < args.Length; source_index++)
     {
-        continue;
+        string argument = args[source_index];
+
+        if (argument.EndsWith(".exe") || outputInfo.IsOutputParameter(argument))
+        {
+            continue;
+        }
+
+        if (File.Exists(argument) && (File.OpenRead(argument) is not null))
+        {
+            sources.Add(argument);
+        }
     }
 
-    sources.Add(args[source_index]);
+    SourcesExplorer explorer = new (sources);
+    LinesResultView artifacts = explorer.GetArtifactsFromSources();
+    outputInfo.PrintArtifacts(artifacts);
 }
-
-SourcesExplorer explorer = new (sources);
-LinesResultView artifacts = explorer.GetArtifactsFromSources();
-outputInfo.PrintArtifacts(artifacts);
 
 stopwatch.Stop();
 Console.WriteLine($"[INFO] Program's Execution Time: {stopwatch.Elapsed}\n");
